e6d89b135d88e24a0a18c48a51b803fc0c1c5aeb
otjake <jaketuriacada@gmail.com>
2020-09-03 05:47:55 +0100

    database index files

--- includes/db.php
+++ includes/db.php
@@ -1,5 +1,11 @@
 <?php
-$conn=mysqli_connect('localhost','root','','blogCMS2.0');
+$cleardb_url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server   = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db       = substr($cleardb_url["path"],1);
+$conn=mysqli_connect($cleardb_server,$cleardb_username,$cleardb_password,$cleardb_db );
+//$conn=mysqli_connect('localhost','root','','blogCMS2.0');
 
 if($conn){
     echo "";
